459c9f639542b5292164d7ca31b3d7ede4af62c1
Brandon Ferrari <ferrarib@uwindsor.ca>
2022-04-01 11:16:07 -0400

    #Generics: Made SimpleStack a Generic Class

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,22 +6,40 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            var stack = new SimpleStack();
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
+        }
+
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack<double>();
             stack.Push(1.2);
             stack.Push(2.8);
             stack.Push(3.0);
 
             double sum = 0.0;
 
-            while(stack.Count > 0)
+            while (stack.Count > 0)
             {
-                double item = stack.Pop();
+                double item = (double)stack.Pop();
                 Console.WriteLine($"Item: {item}");
                 sum += item;
             }
 
             Console.WriteLine($"Sum: {sum}");
-            Console.ReadLine();
+        }
+
+        private static void StackStrings()
+        {
+            var stack = new SimpleStack<string>();
+            stack.Push("Wired Brain Coffee");
+            stack.Push("PluralSight");
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
         }
     }
 }
