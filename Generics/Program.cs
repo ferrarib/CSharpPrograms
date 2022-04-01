2f405226f6c2edbe0924808ec648ae9b7b610138
Brandon Ferrari <ferrarib@uwindsor.ca>
2022-04-01 10:42:28 -0400

    #Generics: SimpleStack Class implementation

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,7 +6,22 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new SimpleStack();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while(stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
         }
     }
 }
