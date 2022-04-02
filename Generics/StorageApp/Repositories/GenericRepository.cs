8679fb3b837e8b1ded18d10c9465a062c9f995d7
Brandon Ferrari <ferrarib@uwindsor.ca>
2022-04-02 18:59:58 -0400

    #Generics: Created base class EntityBase and Interface IEntity

--- Generics/StorageApp/Repositories/GenericRepository.cs
+++ Generics/StorageApp/Repositories/GenericRepository.cs
@@ -1,21 +1,28 @@
 ﻿using StorageApp.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StorageApp.Repositories
 {
-    public class GenericRepository<T>
+    public class GenericRepository<T> where T : IEntity
     {
         private readonly List<T> _items = new List<T>();
 
+        public T GetById(int id)
+        {
+            return _items.Single(item => item.Id == id);
+        }
+
         public void Add(T item)
         {
+            item.Id = _items.Any() ? _items.Max(item => item.Id) + 1 : 1;
             _items.Add(item);
         }
 
         public void Save()
         {
-            foreach(var item in _items)
+            foreach (var item in _items)
             {
                 Console.WriteLine(item);
             }
